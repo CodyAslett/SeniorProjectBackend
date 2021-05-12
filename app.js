5fb7247b1d3884e123ef7ed20edd7d0b2ca1e7ea
CodyAslett <codyweller@byui.edu>
2021-05-12 01:01:35 -0600

    v0.1

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ console.log('server Starting');
 const http = require('http')
 const port = 3000
 
+var express = require('express')
+var app = express()
+
 
 const server = http.createServer(function (req, res) {
     res.write('Hello Node')
@@ -17,4 +20,11 @@ server.listen(port, function (error) {
     } else {
         console.log('Server is listening on port ' + port)
     }
+})
+
+
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world')
 })
