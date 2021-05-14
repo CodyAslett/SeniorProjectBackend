a958857418cc5524830e14b85af96ca6f6e96751
CodyAslett <codyweller@byui.edu>
2021-05-14 12:01:48 -0600

    fixed some errors from seperating api and web

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { Client } = require('pg')
 
 
 var app = express();
-var web = express();
+
 const client = new Client({
     user:
 });
@@ -25,20 +25,6 @@ client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
     console.log(err ? err.stack : res.rows[0].message) // Hello World!
     client.end()
 })
-
-/////////////////////////////////////////////////////////////////////
-// Error Responses
-/////////////////////////////////////////////////////////////////////
-
-//web.use(express.static(__dirname + '/public'));
-web.use(express.static(__dirname + '/public', { extensions: ['html'] }));
-web.set('views', __dirname + '/public/views');
-web.engine('html', require('ejs').renderFile);
-web.set('view engine', 'html');
-
-//postgress
-
-
 
 /*
 const server = http.createServer(function (req, res) {
@@ -62,16 +48,6 @@ app.get('/', function (req, res) {
     res.send('Thank you for your API request to get : ' + JSON.stringify(req.body))
     console.log('Get Requested : ' + JSON.stringify(req.headers))
 })
-
-
-/////////////////////////////////////////////////////////////////////
-// Error Responses
-/////////////////////////////////////////////////////////////////////
-// BAD URL // 404 ERROR respons
-web.get('*', function (req, res) {
-    console.log('BAD 404 Request for ' + req.url);
-    res.send("Say Whay??", 404);
-});
 
 /////////////////////////////////////////////////////////////////////
 // Start
--- web.js
+++ web.js
@@ -6,6 +6,13 @@ var express = require('express');
 var session = require('express-session')
 var bodyParser = require('body-parser')
 
+var web = express();
+
+web.use(express.static(__dirname + '/public', { extensions: ['html'] }));
+web.set('views', __dirname + '/public/views');
+web.engine('html', require('ejs').renderFile);
+web.set('view engine', 'html');
+
 var sess = {
     secret: 'auto',
     cookie: {}
@@ -14,6 +21,8 @@ if (web.get('env') === 'production') {
     web.set('trust proxy', 1) //trust first proxy
     sess.cookie.secure = true //serve secure cookies
 }
+
+
 web.use(session(sess));
 
 web.set('port', (process.env.PORT || webPort))
@@ -23,10 +32,18 @@ web.use(bodyParser.urlencoded({
 
 web.use(bodyParser.json());
 
-// respond with "hello world" when a GET request is made to the homepage
 web.get('/', function (request, response) {
     console.log('pages/index' + request.url + " Request");
     response.render('index.html');
+});
+
+/////////////////////////////////////////////////////////////////////
+// Error Responses
+/////////////////////////////////////////////////////////////////////
+// BAD URL // 404 ERROR respons
+web.get('*', function (req, res) {
+    console.log('BAD 404 Request for ' + req.url);
+    res.send("Say Whay??", 404);
 });
 
 web.listen(webPort, () => {
