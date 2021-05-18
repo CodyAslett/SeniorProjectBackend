c2c953da7f3be3e4757c94046bbb13056b1ffe6d
CodyAslett <codyweller@byui.edu>
2021-05-17 18:53:58 -0600

    catch bad respons

--- app.js
+++ app.js
@@ -93,46 +93,51 @@ app.use(bodyParser.json());
 
 
 app.get('/login', function (request, response) {
-    //response.send('login atemped : ' + request.url);
-    const queryObject = url.parse(request.url, true).query;
-    var user = JSON.stringify(queryObject["username"]).replace(/"/g, "");
-    var pass = JSON.stringify(queryObject["password"]).replace(/"/g, "");
-    var token = hat();
-    
-    var dbRequest;
-    pool.connect((err, client, release) => {
-        if (err) {
-            return console.error('Error acquiring client', err.stack)
-        }
-        var query = "SELECT * FROM users WHERE name = '" + user + "'";
-        console.log('quering : ' + query + '& FROM : ' + JSON.stringify(request.header('x-forwarded-for')));
-        client.query(query, (err, result) => {
-           // release();
+    try {
+        //response.send('login atemped : ' + request.url);
+        const queryObject = url.parse(request.url, true).query;
+        var user = JSON.stringify(queryObject["username"]).replace(/"/g, "");
+        var pass = JSON.stringify(queryObject["password"]).replace(/"/g, "");
+        var token = hat();
+
+        var dbRequest;
+        pool.connect((err, client, release) => {
             if (err) {
-                return console.error('Error executing query', err.stack);
+                return console.error('Error acquiring client', err.stack)
             }
-            if (pass === result.rows[0]['password']) {
-                console.log('loginSucess for ' + user);
-                dbRequest = result.rows[0];
-//                var token = hat();
-                var tokenPost = "INSERT INTO tokens(username, token, ip) VALUES ('" + user + "', '" + token + "', '" + ip.address() + "')";
-                console.log('sending ' + tokenPost);
-                client.query(tokenPost, (err, resultToken) => {
-                    release();
-                    if (err) {
-                        return console.error('Error executing query', err.stack);
-                    }
-                    console.log("sent tokent to db");
-                });
-            }
-            else
-                console.log(result.rows[0]['password'] + '!=' + pass);
-            
+            var query = "SELECT * FROM users WHERE name = '" + user + "'";
+            console.log('quering : ' + query + '& FROM : ' + JSON.stringify(request.header('x-forwarded-for')));
+            client.query(query, (err, result) => {
+                // release();
+                if (err) {
+                    return console.error('Error executing query', err.stack);
+                }
+                if (pass === result.rows[0]['password']) {
+                    console.log('loginSucess for ' + user);
+                    dbRequest = result.rows[0];
+                    //                var token = hat();
+                    var tokenPost = "INSERT INTO tokens(username, token, ip) VALUES ('" + user + "', '" + token + "', '" + ip.address() + "')";
+                    console.log('sending ' + tokenPost);
+                    client.query(tokenPost, (err, resultToken) => {
+                        release();
+                        if (err) {
+                            return console.error('Error executing query', err.stack);
+                        }
+                        console.log("sent tokent to db");
+                    });
+                }
+                else
+                    console.log(result.rows[0]['password'] + '!=' + pass);
+
+            });
         });
-    });
-    var queryPass = dbRequest;
-    response.send(token);
-    console.log('user : ' + user + '   ' + queryPass);
+        var queryPass = dbRequest;
+        response.send(token);
+        console.log('user : ' + user + '   ' + queryPass);
+    }
+    catch (err) {
+        response.send('Bad Request');
+    }
 });
 
 // respond to a GET requests
