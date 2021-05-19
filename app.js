5479ff962f227e44d826a657d8dbfac3cdb440d2
CodyAslett <codyweller@byui.edu>
2021-05-19 15:24:25 -0600

    update some error messages

--- app.js
+++ app.js
@@ -9,79 +9,48 @@ const url = require('url');
 const ip = require('ip');
 const hat = require('hat');
 const { Pool } = require('pg');
-const { token } = require('morgan');
-
-//const { networkInterfaces } = require('os');
-
-//const thenetworkInterfaces = os.networkInterfaces();
-//const network = thenetworkInterfaces['Local Area Connection 3'];
-//const theIP = arr[1].address;
-
-//console.log(theIP);
 
 const apiPort = 3000;
-const pgPort = 5432;
-const dbName = 'syncaudiobookplayerbackenddb';
-const dbUser = 'remotetesting';
-const fixedIP = '54.185.209.208';
-//const fixedIP = 'localhost'
-var dbPass;
 
-dbPass = fs.readFileSync('../pass.txt', 'utf8', function (err, data) {
-    if (err)
-    {
-        return console.log(err);
-    }
-    return data;    
+
+dbPass = fs.readFileSync('../pass.txt', 'utf8', function (err, data)
+{
+   if (err)
+   {
+      return console.log(err);
+   }
+   return data;
 });
 
-var pgConfig = {
-    host: fixedIP,
-    port: pgPort,
-    user: dbUser,
-    password: dbPass,
-    database: dbName,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 20000,
-};
-
-/*
-const pool = new Pool({
-    host: fixedIP,
-    port: pgPort,
-    user: dbUser,
-    password: dbPass,
-    database: dbName,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 20000,
-});
-*/
-//const pool = new Pool(pgConfig);
 const pool = new Pool();
 
 console.log('Pool :', JSON.stringify(pool));
 
 // boot db test
-pool.connect((err, client, release) => {
-    if (err) {
-        return console.error('Error acquiring client', JSON.stringify(pool), err.stack, err.message)
-    }
-    client.query('SELECT NOW()', (err, result) => {
-        if (err) {
-            return console.error('Error executing query', err.stack);
-        }
-        console.log(result.rows);
-    });
-    client.query('SELECT COUNT(*) FROM users', (err, result) => {
-        if (err) {
-            return console.error('Error executing query', err.stack);
-        }
-        console.log('Users found : ', result.rows[0]['count']);
-    });
+pool.connect((err, client, release) =>
+{
+   if (err)
+   {
+      return console.error('Error acquiring client', JSON.stringify(pool), err.stack, err.message)
+   }
+   client.query('SELECT NOW()', (err, result) =>
+   {
+      if (err)
+      {
+         return console.error('Error executing query', err.stack);
+      }
+      console.log(result.rows);
+   });
+   client.query('SELECT COUNT(*) FROM users', (err, result) =>
+   {
+      if (err)
+      {
+         return console.error('Error executing query', err.stack);
+      }
+      console.log('Users found : ', result.rows[0]['count']);
+   });
 
-    release();
+   release();
 });
 
 
@@ -92,140 +61,151 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get('/login', function (request, response) {
-    try {
-        //response.send('login atemped : ' + request.url);
-        const queryObject = url.parse(request.url, true).query;
-        var token = hat();
-        var dbRequest;
-        var tokenResult;
+app.get('/login', function (request, response)
+{
+   try
+   {
+      //response.send('login atemped : ' + request.url);
+      const queryObject = url.parse(request.url, true).query;
+      // TODO: Add checks for duplicate tokens
+      var token = hat();
+      var dbRequest;
+      var tokenResult;
 
 
-        if (queryObject["token"] !== undefined && queryObject["token"] !== null) {
-            pool.connect((err, client, release) =>
+      if (queryObject["token"] !== undefined && queryObject["token"] !== null)
+      {
+         pool.connect((err, client, release) =>
+         {
+            if (err)
             {
-                if (err)
-                {
-                    return console.error('Error acquiring token client', err.stack)
-                }
-
-                var userGivenToken = JSON.stringify(queryObject["token"]).replace(/"/g, "");
-                var query = "SELECT (username, token) FROM tokens WHERE token = '" + userGivenToken + "'";
-
-                console.log('making dbRequest : ' + query);
-
-                client.query(query, (err, result) =>
-                {
-                    if (err)
-                    {
-                        return console.error('Error executing token query', err.stack);
-                    }
-                    tokenResult = ((result.rows[0]['row']).replace(/\)|\(/g, "")).split(',');
-                    var dbToken = tokenResult[1];
-                    var dbUser = tokenResult[0];
-                    var user;
-
-                    console.log(typeof (dbToken));
-
-                    console.log('DB request result : ' + dbToken);
-                    if (queryObject["username"] !== undefined && queryObject["username"] !== null) {
-                        user = JSON.stringify(queryObject["username"]).replace(/"/g, "");
-                    }
-
-                    if (dbToken === userGivenToken && user === dbUser) {
-                        console.log('token match ' + userGivenToken)
-                        response.send('ACCEPTED');
-                        return;
-                    }
-                    else {
-                        console.log('Token Mismatch : ' + dbToken + ' != ' + userGivenToken);
-                        respond.send('DENIED');
-                        return;
-                    }
-                });
-            });
-        }
-
-
-
-
-        if (queryObject["username"] !== undefined && queryObject["username"] !== null) {
-            var user = JSON.stringify(queryObject["username"]).replace(/"/g, "");
-            if (queryObject["password"] !== undefined && queryObject["password"] !== null) {
-                var pass = JSON.stringify(queryObject["password"]).replace(/"/g, "");
-
-                pool.connect((err, client, release) => {
-                    if (err) {
-                        return console.error('Error acquiring user client', err.stack)
-                    }
-                    var query = "SELECT (password) FROM users WHERE name = '" + user + "'";
-                    console.log('quering : ' + query + '& FROM : ' + request.connection.remoteAddress);
-                    client.query(query, (err, result) => {
-                        // release();
-                        if (err) {
-                            return console.error('Error executing query', err.stack);
-                        }
-                        console.log('Result ' + JSON.stringify(result));
-                       if (result.rows.length > 0)
-                       {
-                          if (pass === result.rows[0]['password'])
-                          {
-                             console.log('loginSucess for ' + user);
-                             dbRequest = result.rows[0];
-                             var tokenPost = "INSERT INTO tokens(username, token, ip) VALUES ('" + user + "', '" + token + "', '" + ip.address() + "')";
-                             console.log('sending ' + tokenPost);
-                             client.query(tokenPost, (err, resultToken) => 
-                             {
-                                release();
-                                if (err) 
-                                {
-                                   return console.error('Error executing query', err.stack);
-                                }
-                                console.log("sent tokent to db");
-                             });
-                          }
-                          else 
-                          {
-                             console.log(result.rows[0]['password'] + '!=' + pass);
-                             response.send('DENIED: PROVIDED USERNAM AND PASSWORD DON\'t MATCH RECORDS');
-                             return;
-                          }
-                       }
-                       else
-                       {
-                          console.log('ERROR : DB requestresult empty :' + result);
-                          response.send('DENIED: PROVIDED USERNAM AND PASSWORD DON\'t MATCH RECORDS');
-                          return;
-                       }
-
-
-                        var queryPass = dbRequest;
-                        response.send('ACCEPTED: ' + token);
-                        console.log('user : ' + user + '   ' + queryPass);
-                    });
-                });
+               return console.error('Error acquiring token client', err.stack)
             }
-        }
-        else 
-        {
-            response.send('DENIED');
-        }
-    }
-    catch (err) 
-    {
-        response.send('ERROR: Bad login Request ' + err.stack);
-    }
+
+            var userGivenToken = JSON.stringify(queryObject["token"]).replace(/"/g, "");
+            var query = "SELECT (username, token) FROM tokens WHERE token = '" + userGivenToken + "'";
+
+            console.log('making dbRequest : ' + query);
+
+            client.query(query, (err, result) =>
+            {
+               if (err)
+               {
+                  return console.error('Error executing token query', err.stack);
+               }
+               tokenResult = ((result.rows[0]['row']).replace(/\)|\(/g, "")).split(',');
+               var dbToken = tokenResult[1];
+               var dbUser = tokenResult[0];
+               var user;
+
+               console.log(typeof (dbToken));
+
+               console.log('DB request result : ' + dbToken);
+               if (queryObject["username"] !== undefined && queryObject["username"] !== null)
+               {
+                  user = JSON.stringify(queryObject["username"]).replace(/"/g, "");
+               }
+
+               if (dbToken === userGivenToken && user === dbUser)
+               {
+                  console.log('token match ' + userGivenToken)
+                  response.send('ACCEPTED');
+                  return;
+               }
+               else
+               {
+                  console.log('Token Mismatch : ' + dbToken + ' != ' + userGivenToken);
+                  respond.send('DENIED');
+                  return;
+               }
+            });
+         });
+      }
+      if (queryObject["username"] !== undefined && queryObject["username"] !== null) 
+      {
+         var user = JSON.stringify(queryObject["username"]).replace(/"/g, "");
+         if (queryObject["password"] !== undefined && queryObject["password"] !== null) 
+         {
+            var pass = JSON.stringify(queryObject["password"]).replace(/"/g, "");
+
+            pool.connect((err, client, release) => 
+            {
+               if (err) 
+               {
+                  return console.error('Error : acquiring user client', err.stack)
+               }
+               var query = "SELECT (password) FROM users WHERE name = '" + user + "'";
+               console.log('quering : ' + query + '& FROM : ' + request.connection.remoteAddress);
+               client.query(query, (err, result) =>
+               {
+                  // release();
+                  if (err) 
+                  {
+                     return console.error('Error : executing query', err.stack);
+                  }
+                  console.log('Result ' + JSON.stringify(result));
+                  if (result.rows.length > 0)
+                  {
+                     if (pass === result.rows[0]['password'])
+                     {
+                        console.log('loginSucess for ' + user);
+                        dbRequest = result.rows[0];
+                        var tokenPost = "INSERT INTO tokens(username, token, ip) VALUES ('" + user + "', '" + token + "', '" + ip.address() + "')";
+                        console.log('sending ' + tokenPost);
+                        client.query(tokenPost, (err, resultToken) => 
+                        {
+                           release();
+                           if (err) 
+                           {
+                              return console.error('Error executing query', err.stack);
+                           }
+                           console.log("sent tokent to db");
+                        });
+                     }
+                     else 
+                     {
+                        console.log(result.rows[0]['password'] + '!=' + pass);
+                        response.send('DENIED : PROVIDED USERNAM AND PASSWORD DON\'t MATCH RECORDS');
+                        return;
+                     }
+                  }
+                  else
+                  {
+                     console.log('ERROR : DB requestresult empty :' + JSON.stringify(result));
+                     response.send('DENIED : PROVIDED USERNAM AND PASSWORD DON\'t MATCH RECORDS');
+                     return;
+                  }
+
+
+                  var queryPass = dbRequest;
+                  response.send('ACCEPTED: ' + token);
+                  console.log('user : ' + user + '   ' + queryPass);
+               });
+            });
+         }
+      }
+      else 
+      {
+         response.send('DENIED');
+      }
+   }
+   catch (err) 
+   {
+      response.send('ERROR: Bad login Request ' + err.stack);
+   }
 });
 
 // respond to a GET requests
-app.get('/', function (req, res) {
-    res.send('Thank you for your API request to get : ' + JSON.stringify(req.body))
-    console.log('Get Requested : ' + JSON.stringify(req.headers))
+app.get('/', function (req, res)
+{
+   res.send('Thank you for your API request to get : ' + JSON.stringify(req.body))
+   console.log('Get Requested : ' + JSON.stringify(req.headers))
 })
 
 /////////////////////////////////////////////////////////////////////
 // Start
 /////////////////////////////////////////////////////////////////////
-app.listen(apiPort, () => {
-    console.log('Example app listening on port : ' + apiPort)
+app.listen(apiPort, () => 
+{
+   console.log('Example app listening on port : ' + apiPort)
 })
