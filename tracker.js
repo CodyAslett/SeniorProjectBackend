998848ac93b5e7d524c8a5aca2a084e129633b61
CodyAslett <codyweller@byui.edu>
2021-05-20 23:06:57 -0600

    updated tracker to more  so conform to bittorrent-tracker example

--- tracker.js
+++ tracker.js
@@ -5,32 +5,13 @@ var server = new Server({
    udp: true, // enable udp server? [default=true]
    http: true, // enable http server? [default=true]
    ws: true,   // enable websocket server? [default=true]
-   stats: true, // enable web-based statistics? [default=true]
-   filter: function (infoHash, params, cb)
-   {
-      // Blacklist/whitelist function for allowing/disallowing torrents. If this option is
-      // omitted, all torrents are allowed. It is possible to interface with a database or
-      // external system before deciding to allow/deny, because this function is async.
-
-      // It is possible to block by peer id (whitelisting torrent clients) or by secret
-      // key (private trackers). Full access to the original HTTP/UDP request parameters
-      // are available in `params`.
-
-      // This example only allows one torrent.
-
-      var allowed = (infoHash === 'aaa67059ed6bd08362da625b3ae77f6f4a075aaa')
-      if (allowed)
-      {
-         // If the callback is passed `null`, the torrent will be allowed.
-         cb(null)
-      } else
-      {
-         // If the callback is passed an `Error` object, the torrent will be disallowed
-         // and the error's `message` property will be given as the reason.
-         cb(new Error('disallowed torrent'))
-      }
-   }
+   stats: true // enable web-based statistics? [default=true]
 })
+
+// Internal http, udp, and websocket servers exposed as public properties.
+server.http
+server.udp
+server.ws
 
 server.on('error', function (err)
 {
