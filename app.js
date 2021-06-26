47da563866ec4f0fb55bc25c9a5081b993042fe0
CodyAslett <codyweller@byui.edu>
2021-06-26 14:06:16 -0600

    5

--- app.js
+++ app.js
@@ -344,7 +344,7 @@ app.get('/getfiles', function (request, response)
                      {
                         console.log("getfile : getting info for : " + JSON.stringify(result.rows[i]));
                         var file = {
-                           id: result.rows[i]
+                           id: result.rows[i].row[0]
                         };
                         torrentFiles.files.push(file);
                      }
