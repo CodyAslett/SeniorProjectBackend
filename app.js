5a4f30068f5a441bf7e0c3956d91f23f88c6c17e
CodyAslett <codyweller@byui.edu>
2021-06-26 14:18:52 -0600

    getfiles : change fields name to id

--- app.js
+++ app.js
@@ -343,9 +343,8 @@ app.get('/getfiles', function (request, response)
                      for (var i = 0; i < result.rowCount; i++)
                      {
                         console.log("getfile : getting info for : " + JSON.stringify(result.fields[i]));
-                        var dbId = result.fields[i].name;
                         var file = {
-                           id: result.fields[i].name
+                           id: result.fields[i].id
                         };
                         torrentFiles.files.push(file);
                      }
